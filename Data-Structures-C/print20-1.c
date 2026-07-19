c8bbcc73aa85a93852663d3d454e81d691476227
abhi-vmlinuz <josephinevincent4@gmail.com>
2026-07-19 17:57:54 +0530

    update exercises

---
+++ Data-Structures-C/print20-1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+{
+	int n=20;
+	for(int i=n;i>=1;i--){
+		printf("%d\n",i);
+	}
+}
---
+++ Data-Structures-C/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main()
+	{
+		int n=10;
+		for(int i = 1;i<=n;i++){
+			printf("%d\n",i*i);
+		}
+	}
